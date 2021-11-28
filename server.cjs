cd8cfbc41bde743131e2007ab5b68c0fd23d391d
NegativeSector <NegativeSector@protonmail.com>
2021-11-28 14:34:30 -0500

    changed server code

--- server.cjs
+++ server.cjs
@@ -2,4 +2,4 @@ const express = require("express")
 const app = express();
 const port = 3000;
 app.use(express.static("dist"));
-app.listen(process.env.port || 8000, () => console.log("server up"))
+app.listen(process.env.PORT|| 8000, () => console.log("server up"))
